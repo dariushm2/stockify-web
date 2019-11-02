684e4f21aade148ad3fd7822c0c6bc4860061152
Dariush Malek <dariushm3@gmail.com>
2019-11-02 19:40:47 -0400

    Updates

---
+++ precache-manifest.24c16e2a587e23af0aa214b63f892648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937aad56b85e2e7cab1056d704298669",
+    "url": "/stockify-web/index.html"
+  },
+  {
+    "revision": "24ff97d26731152c55de",
+    "url": "/stockify-web/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "24ff97d26731152c55de",
+    "url": "/stockify-web/static/js/2.9b70284f.chunk.js"
+  },
+  {
+    "revision": "e72bd6aa036757b3744d",
+    "url": "/stockify-web/static/js/main.0aca8bdd.chunk.js"
+  },
+  {
+    "revision": "7f38c6e86501b0db1a3c",
+    "url": "/stockify-web/static/js/runtime-main.bafd4a8d.js"
+  }
+]);
